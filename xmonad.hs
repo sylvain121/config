291ec63048135308ee485a2d8abd4179880ec821
Sylvain121 <sylvain121@gmail.com>
2016-04-04 17:05:26 +0200

    update

--- xmonad.hs
+++ xmonad.hs
@@ -1,8 +1,16 @@
 import XMonad
 import XMonad.Config.Azerty
+import XMonad.Util.EZConfig(additionalKeys)
 
-main = xmonad azertyConfig
-	{ terminal    = "urxvt"
+main = xmonad $ azertyConfig
+	{ 
+	terminal    = "urxvt"
 	, modMask     = mod4Mask
 	, borderWidth = 3
 	}
+	`additionalKeys`
+	[
+	((0                     , 0x1008FF11), spawn "amixer set Master 2-"),
+	((0                     , 0x1008FF13), spawn "amixer set Master 2+"),
+	((0                     , 0x1008FF12), spawn "amixer set Master toggle")
+	]
